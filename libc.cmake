9b6df4a54b2d3c015679e39c1ba2839f6c8d40f8
dominic <mail@dominicp.de>
2025-09-16 23:55:46 +0200

    formatting

--- libc.cmake
+++ libc.cmake
@@ -1,18 +1,9 @@
-set(
-  LIBC_SOURCE_FILES
-  src/string/memcpy.cpp
-  src/string/memset.cpp
-  src/string/strlen.cpp
-)
+set(LIBC_SOURCE_FILES src/string/memcpy.cpp src/string/memmove.cpp src/string/memset.cpp src/string/strlen.cpp)
 
-set(libc_flags
-    -Wno-unsafe-buffer-usage
-    -DLIBC_COPT_PUBLIC_PACKAGING
-)
+set(libc_flags -DLIBC_COPT_PUBLIC_PACKAGING)
 
 list(JOIN libc_flags " " LIBC_FLAGS)
 
 list(TRANSFORM LIBC_SOURCE_FILES PREPEND "${CMAKE_CURRENT_LIST_DIR}/")
 
-set_source_files_properties(
-  ${LIBC_SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${LIBC_FLAGS}")
+set_source_files_properties(${LIBC_SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${LIBC_FLAGS}")
